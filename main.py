6b212015bbf26e3e0fde9560671c0e301ccbefad
Kyohei Oka <0622okakyo@gmail.com>
2021-01-15 15:57:54 +0900

    動作確認

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
+
 @st.cache
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
